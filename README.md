777badf636d632ad792093a2b6a5ce23f2546b34
natanree <natanreee@gmail.com>
2023-12-16 16:57:57 -0800

    more initial additions

---
+++ README.md
@@ -0,0 +1,10 @@
+## Web Blog App
+
+Author: Yu Chung "Nathan" Lee
+
+Code is based off arslanay smile app starter code
+
+### To run the application
+```
+py app.py
+```
--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from __future__ import print_function
+
+import sys
+
+from flask import Blueprint, render_template, flash, redirect, url_for, request
 from config import Config
+from flask_login import login_required, current_user
 from flask import current_app as app
+
 from app import db
 
 bp_routes = Blueprint('routes', __name__)
 bp_routes.template_folder = Config.TEMPLATE_FOLDER
+
+@bp_routes.route('/', methods=['GET'])
+@bp_routes.route('/index',methods=['GET'])
+@login_required
+def index():
+    return render_template('index.html')
